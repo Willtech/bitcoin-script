63ea0eac759b8c0fe60603a75e59a11bbd078ab3
Damian Williamson <willtech@live.com.au>
2022-02-26 14:24:37 +1100

    initial commit
    Signed-off-by: Damian Williamson <willtech@live.com.au>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bitcoin-script
-A collection of files related to bitcoind
+**A collection of files related to bitcoind**
+
+## bitcoin.conf
+A version of bitcoin.conf with lots of the useful settings.
+
+##runbitcoind.sh
+A bsah script to run bitcoind and after some period exit and when finished exiting to restart.
---
+++ runbitcoind.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Exit and restart bitcoind after some time period
+# Willtech ©2022
+
+# How often to restart in seconds
+timeout=3600
+
+# BAT / CMD goto function
+# Source: https://www.codegrepper.com/code-examples/shell/bash+jump+to
+function goto
+{
+    label=$1
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+# Just for the heck of it: how to create a variable where to jump to:
+start=${1:-"start"}
+goto "$start"
+
+: start
+
+bitcoind -daemon
+sleep $timeout
+bitcoin-cli stop
+until timeout 10s tail -f /media/drive2/.bitcoin/debug.log | grep -m 1 "Shutdown: done"
+  do
+    sleep 2
+  done
+
+echo again
+
+goto "start"
+
+echo bar
